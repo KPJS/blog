078c5f270c607ce73821bf39175be4fc56fbd589
Ján Neščivera <jan.nescivera@gmail.com>
2015-08-27 17:29:40 +0200

    generation of list of posts

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 var port = process.env.PORT || 1337;
-var http = require('http');
-var templateStart = "<html><head><meta charset=\"UTF-8\"></head><body><div>";
-var templateEnd = "</div></body></html>";
-var fs = require('fs');
-http.createServer(function (req, res) {
-  if(/.png$/.test(req.url)) {
-    res.writeHead(200, {'Content-Type': 'image/png'});
-    readFile('.' + req.url, function(content) {
-      res.end(content);
-    });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    readFile('1st-post.txt', function(content) {
-      res.end(templateStart + content + templateEnd);
-    });
-  }
-}).listen(port);
-console.log('Server running on '+ port);
 
-function readFile(path, callback) {
-  fs.readFile(path, function(err, content) {
-    if(err)
-      console.log(err);
-    callback(content);
-  });
-}
+var express = require('express');
+var hbs = require('hbs');
+var fs = require('fs');
+
+var app = express();
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+app.use(express.static('static'));
+
+app.get('/', function(req, res) {
+    fs.readdir('static/posts', function(err, files){
+        res.render('index.html', { posts: files });
+    });
+});
+app.listen(port);
+console.log('Server running on '+ port);
