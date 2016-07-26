172d63b351e92afe1dd79e4aae51fad7a91cd100
pethO <petho@temnota.sk>
2016-07-26 22:53:34 +0200

    moved deleting of post into post edit instead of post read

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ module.exports = function(logger, authenticationController, postsController, use
 		app.get('/contact', postsController.contactRouteHandler);
 		app.get('/posts', postsController.getPostsRouteHandler);
 		app.get('/posts/:uri', postsController.getReadRouteHandler);
-                app.delete('/posts/:uri', verifyRulerOrOwner, postsController.deletePostRouteHandler);
+		app.delete('/edit/:uri', verifyRulerOrOwner, postsController.deletePostRouteHandler);
 		app.get('/edit/:uri', verifyRulerOrOwner, postsController.getEditRouteHandler);
 		app.post('/edit/:uri', verifyRulerOrOwner, postsController.postEditRouteHandler);
 		app.get('/create', verifyCitizen, postsController.getCreateRouteHandler);
